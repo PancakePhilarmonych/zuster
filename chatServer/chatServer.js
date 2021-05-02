ed363216bf810243dfce299f299684eaba048140
PancakePhilarmonych <rikko.alexandr@gmail.com>
2021-05-02 14:48:02 +0000

    update app from server

--- chatServer/chatServer.js
+++ chatServer/chatServer.js
@@ -4,42 +4,35 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
 const startServer = (port) => {
+  app.use(express.static(__dirname + '/node_modules'));
+
   app.get('/chat', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+    console.log("Server lOG");
   });
 
-  // io.on('connection', (socket) => {
-  //   console.log('a user connected');
+   io.on('connection', (socket) => {
+     console.log('a user connected');
 
-  //   socket.on('disconnect', () => {
-  //     console.log('user disconnected');
-  //   });
+     socket.on('disconnect', () => {
+       console.log('user disconnected');
+     });
 
-  //   socket.on('chat message', (msg) => {
-  //     console.log('message: ' + msg);
-  //   });
-  // });
+     socket.on('chat message', (msg) => {
+       console.log('message: ' + msg);
+     });
 
-  // io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+     socket.on('chat message', (msg) => {
+       io.emit('chat message', msg);
+     });
 
-  // io.on('connection', (socket) => {
-  //   socket.broadcast.emit('hi');
-  // });
-
-  // io.on('connection', (socket) => {
-  //   socket.on('chat message', (msg) => {
-  //     io.emit('chat message', msg);
-  //   });
-  // });
-
-  io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-      io.emit('chat message', msg);
-    });
-  });
+     socket.broadcast.emit('hi');
+   });
 
   http.listen(port, () => {
+    console.log(io)
     console.log(`listening on *:${port}`);
   });
 }
