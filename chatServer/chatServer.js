426cecf33e9e7358231d5ad498d640a40cc15cbe
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-05-03 22:01:23 +0300

    complete 1.0.0 version of chat && add VUE && add styles

--- chatServer/chatServer.js
+++ chatServer/chatServer.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const startServer = (port) => {
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
